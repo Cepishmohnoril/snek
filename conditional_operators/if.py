8f97f977dcc5d9b25177a2ba68de65bbbdf2aee7
Vladyslav Radchuk <cepishmohnoril@gmail.com>
2023-07-07 15:12:19 +0300

    some logical operators and loops

---
+++ conditional_operators/if.py
@@ -0,0 +1,14 @@
+age = int(input("How old are you?: "))
+
+if age >= 18:
+    print("Go find yourself a job!")
+elif age <= 0:
+    print("Impossiburu")
+elif age == 100:
+    print("Old fuck!")
+else:
+    print("Talk to me when you are 18.")
+
+
+
+#logical operators in python are 'and', 'or', 'not'
--- variables/strings.py
+++ variables/strings.py
@@ -10,3 +10,22 @@ print(name.isalpha())
 print(name.count("o"))
 print(name.replace("y", "m"))
 print(name * 3)
+
+
+#Slice
+
+first_name = name[:4] #0:4
+print(first_name)
+
+last_name = name[5:]
+print(last_name)
+
+dumm_name = name[::2]
+print(dumm_name) #even bleed?
+
+reverse_name = name[::-1]
+print(reverse_name)
+
+website = "https://goole.com"
+slice = slice(8, -4)
+print(website[slice])
